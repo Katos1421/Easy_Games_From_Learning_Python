39b58e2925df835b4554f8d95ff750a141fdf69b
Danil <142499333+Katos1421@users.noreply.github.com>
2024-06-02 19:56:41 +0300

    Update Эксклюзивная сеть (ВЫПОЛНЕНО).py

---
+++ Глава 3/Эксклюзивная сеть (ВЫПОЛНЕНО).py
@@ -0,0 +1,29 @@
+# Программа "Эксклюзивная сеть"
+# Попытка воссоздать что-то вроде Фейсбука или даже Вконтакте. По-крайней мере, попытка - не пытка :)
+print('\tЭксклюзивная компьютерная сеть')
+print('\tТолько для зарегистрированных пользователей')
+security = 0
+username = ""
+while not username:
+    username = input('Логин: ')
+password = ""
+while not password:
+    password = input('Пароль: ')
+if username == "D. Voronin" and password == "KatoS":
+    print ('Добро пожаловать, Катос')
+    security = 5
+elif username == "S. Meier" and password == "Civilization":
+    print ('Добро пожаловать, Сид')
+    security = 3
+elif username == "S. Miyamoto" and password == "Mariobros":
+    print ('Доброго времени суток, Сигеру')
+    security = 3
+elif username == "W. Wright" and password == "thesims":
+    print ('Как дела, Уилл?')
+    security = 3
+elif username == "guest" and password == "guest":
+    print ('Добро пожаловать к нам в гости!')
+    security = 1
+else:
+    print ('Войти в систему не удалось. Возможно, не такой уж вы и эксклюзивный гражданин...\n')
+input('\n\nНажмите Enter, чтобы выйти')
